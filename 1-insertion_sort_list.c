75c9cfc2cc6519c89363b321acbea4defea5b950
Oloishorua <jacoboloishorua@gmail.com>
2024-02-21 01:11:42 +0300

    Task 1 - Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ *	in asceding order using the insertion list algorithm
+ * @list: the doubly linked list to sort
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *pntr;
+
+	if (!list)
+		return;
+
+	pntr = *list;
+
+	while (pntr)
+	{
+		while (pntr->next && (pntr->n > pntr->next->n))
+		{
+			temp = pntr->next;
+			pntr->next = tmp->next;
+			temp->prev = pntr->next;
+
+			if (temp->prev)
+				pntr->prev->next = temp;
+
+			if (temp->next)
+				temp->next->prev = pntr;
+
+			pntr->prev = temp;
+			temp->next = pntr;
+
+			if (temp->prev)
+				pntr = temp->prev;
+			else
+				*list = temp;
+
+			print_list(*list);
+		}
+		pntr = pntr->next;
+	}
+}
